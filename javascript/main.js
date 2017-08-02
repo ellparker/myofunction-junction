360a6b7baed33f8d4cde50098b4b324e8b6ca3dc
Ell Parker <miss.ellparker@gmail.com>
2017-08-01 21:39:46 -0700

    second commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,13 @@
         $(document).ready(function(){
-            $(".dropdown-menu li a").click(function(){
+            $(".dropdown-menu li a").hov(function(){
             $("#dropdownMenu1").text($(this).text());
             });
         });
 
-
-
-
-
-
-
-        //  $("list-group-ankle").show()
-        //  $("list-group-forearm").show()
-        //  $("list-group-clavicle").show()
+        
+  $(document).ready(function(){
+    $('ul.first').bsPhotoGallery({
+      "classes" : "col-lg-2 col-md-4 col-sm-3 col-xs-4 col-xxs-12",
+      "hasModal" : true
+    });
+  });
