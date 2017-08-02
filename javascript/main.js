c9b104814f52aebce0835310373d4b4b8097133b
Ell Parker <miss.ellparker@gmail.com>
2017-08-01 19:10:24 -0700

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+        $(document).ready(function(){
+            $(".dropdown-menu li a").click(function(){
+            $("#dropdownMenu1").text($(this).text());
+            });
+        });
+
+
+
+
+
+
+
+        //  $("list-group-ankle").show()
+        //  $("list-group-forearm").show()
+        //  $("list-group-clavicle").show()
